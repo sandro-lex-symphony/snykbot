76c75d1aa55ed90209508e0ca18ea9384c096bcc
Sandro Lex <sandro.lex@symphony.com>
2021-03-31 14:47:00 +0200

    clean up

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/sandro-lex-symphony/gobot"
 	"github.com/sandro-lex-symphony/snykbot/processors"
 )
 
 func main() {
 	gobot.Init()
-	fmt.Println("XXXXX")
 	gobot.Loop(processors.Experiment)
 }
